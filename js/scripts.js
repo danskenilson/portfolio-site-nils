835266621847c4967af80a6275d3c368ebe6dfba
danskenilson <113185423+danskenilson@users.noreply.github.com>
2022-09-26 22:28:56 +0200

    Added nav code to script file

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hey there and welcome to my website");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
